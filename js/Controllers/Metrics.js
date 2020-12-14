4ea8219e2ad4a8c61a8adee76e34647bcdb61dc8
jerrolb <jerrolb@gmail.com>
2020-12-13 20:49:18 -0800

    improve metrics logging

--- js/Controllers/Metrics.js
+++ js/Controllers/Metrics.js
@@ -1,6 +1,6 @@
-import Token from '../Token';
 import {URL} from '../Constants';
 import store from '../redux/store/index';
+import Token from '../Token';
 
 const options = {
   method: 'POST',
@@ -16,15 +16,43 @@ const generateParams = (params) => {
   )).join('&');
 };
 
-const recordView = ({id, title, url}) => {
+const getTimestamp = () => {
+  const now = new Date();
+  return `${now.toDateString()} ${now.toTimeString()}`;
+}
+
+const getDuration = () => {
+  const appStartTime = store.getState().appStartTime;
+  const seconds = Math.ceil((Date.now() - appStartTime) / 1000);
+  let mins;
+  let hours;
+
+  if (!seconds) {
+      return "1s";
+  }
+
+  if (seconds < 60) {
+      return `${seconds}s`;
+  }
+
+  if (seconds < 3600) {
+      return `${Math.floor(seconds / 60)}m ${seconds % 60}s`;
+  }
+
+  mins = Math.floor(seconds / 60);
+  hours = Math.floor(mins / 60);
+  mins %= 60;
+  return `${hours}h ${mins}m ${seconds % 60}s`;
+}
+
+const recordView = ({id, title}) => {
   if (isDevMode()) {
     return;
   }
   const params = generateParams({
     id: id,
     title: title,
-    url: url,
-    timestamp: new Date().toGMTString(),
+    timestamp: getTimestamp(),
     token: Token,
   });
   const urlWithParams = `${URL.METRICS}?${params}`;
@@ -35,11 +63,9 @@ const recordSession = () => {
   if (isDevMode()) {
     return;
   }
-  const appStartTime = store.getState().appStartTime;
-  const seconds = Math.ceil((Date.now() - appStartTime) / 1000);
   const params = generateParams({
-    seconds: seconds,
-    timestamp: new Date().toGMTString(),
+    duration: getDuration(),
+    timestamp: getTimestamp(),
     token: Token,
   });
   const urlWithParams = `${URL.METRICS}?${params}`;
